335c76f90752074f2e7487d134fc9b85949b22d8
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-17 10:14:33 -0300

    docs: 📑  Adicionando sons aos botões.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
@@ -15,7 +15,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio)
+
+    tecla.onclick = function () {
+    tocaSom(idAudio);
+    }
     contador = contador + 1;
     console.log(contador);
 }
